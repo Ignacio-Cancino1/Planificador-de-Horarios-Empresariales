3693fa67d04060e5d36f4199d4a98b9e6d042de5
Ignacio Cancino <icancino2021@alu.uct.cl>
2025-05-11 14:07:37 -0400

    ad

--- planificador-horario/src/services/auth.js
+++ planificador-horario/src/services/auth.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL
-// Asegúrate de tener esta variable en tu .env
 });
 
 export const login = async (loginData) => {
